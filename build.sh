49b7f0b08fe45f36ba5b3137b9108dea626a2943
RaghuVarma331 <raghuvarma331@gmail.com>
2020-08-24 19:08:40 +0000

    kernel: compile using clang
    Signed-off-by: RaghuVarma331 <raghuvarma331@gmail.com>

--- build.sh
+++ build.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
 echo "------------------------------------------------------"
-echo "Kernel Source For FIH-SDM660-2018 Models"
-echo "Lets Compile kernel with GCC. sit Back & Have A coffee "
+echo "Kernel Source For Nokia 6.1 Plus"
+echo "Lets Compile kernel with Clang. sit Back & Have A coffee "
 echo "------------------------------------------------------"
 
-path=
 password=
+pathtoclang=/root/clang
+pathtogcc=/root/aarch64-linux-android-4.9
+path=/root
 
 cd
 mkdir black_caps
 git clone https://github.com/RaghuVarma331/aarch64-linux-android-4.9.git -b master --depth=1 aarch64-linux-android-4.9
+git clone https://github.com/RaghuVarma331/clang.git -b android-9.0 --depth=1 clang
 cd kernel
 clear
-export ARCH=arm64
-export CROSS_COMPILE=$path/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-mkdir output
-make -C $(pwd) O=output SAT-perf_defconfig
-make -j32 -C $(pwd) O=output
-cp -r output/arch/arm64/boot/Image.gz-dtb $path/kernel/DRG_sprout
+make O=out ARCH=arm64 SAT-perf_defconfig
+PATH=$pathtoclang/bin:$pathtogcc/bin:${PATH} \
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE=aarch64-linux-android-
+cp -r out/arch/arm64/boot/Image.gz-dtb $path/kernel/DRG_sprout
 cd DRG_sprout
-zip -r Black_caps-9.0-GCC-FIH-SDM660-2018-$(date +"%Y%m%d").zip META-INF patch tools Image.gz-dtb anykernel.sh
-cp -r Black_caps-9.0-GCC-FIH-SDM660-2018-$(date +"%Y%m%d").zip $path/black_caps
+zip -r Black_caps-9.0-CLANG-FIH-SDM660-2018-$(date +"%Y%m%d").zip META-INF patch tools Image.gz-dtb anykernel.sh
+cp -r Black_caps-9.0-CLANG-FIH-SDM660-2018-$(date +"%Y%m%d").zip $path/black_caps
 cd
 cd $path
 cd black_caps
